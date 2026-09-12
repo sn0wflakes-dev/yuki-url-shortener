796fda1914a7e37a3d78ccd2815ec2efa2602e62
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-09-12 19:22:28 +0700

    fix: add server port config to vite config file

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+
   plugins: [
     react(),
     tailwindcss()
