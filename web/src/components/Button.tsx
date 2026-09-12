20a2b1972be6f9b12210d38275d502a79b208a4e
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-09-12 19:19:48 +0700

    fix: fixing input and button component

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -4,15 +4,11 @@ type ButtonProps = PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>;
 
 const Button = ({onClick, disabled, children, type="button", className = "", ...props}: ButtonProps) => {
 
-  const disableState = disabled 
-    ? 'bg-ctp-yellow-200 text-ctp-subtext1' 
-    : 'bg-ctp-yellow-500 text-ctp-crust hover:bg-ctp-yellow-600';
-
   return (
     <button
     type={type}
     onClick={onClick}
-    className={`custom-btn ${disableState} ${className}`}
+    className={`custom-btn ${className}`}
     {...props}
     >
     {children}
